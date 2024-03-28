2b33603f651d4f1ad8b4bc7fc7da3555a493f5c7
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2024-03-29 02:53:14 +0600

    Banner Issue Fixed

--- src/pages/LandingBanner.jsx
+++ src/pages/LandingBanner.jsx
@@ -1,4 +1,3 @@
-
 import { Carousel, Typography } from "@material-tailwind/react";
 import sliderImage1 from "../assets/banner/loan_slider_image (1).jpg";
 import sliderImage2 from "../assets/banner/loan_slider_image (2).jpg";
@@ -31,32 +30,32 @@ export default function LandingBanner() {
   return (
     <Carousel className="lg:h-[80vh]" navigation={false}>
       {sliders.map((s, i) => (
-        <div key={i} className="relative h-full w-full">
-          {/* <img
-            src={s.banner}
-            alt="Hero_Image"
-            className="h-full w-full object-cover"
-          /> */}
-          <div className={`absolute inset-0 h-full w-full flex justify-center items-center  bg-gradient-to-r ${ i % 2 !== 0 ? "from-primary via-[#34327D] to-primary" : "from-blue-900 via-blue-400 to-blue-900" }`}>
-            <div className="text-center w-3/4 lg:w-3/5">
-              <Typography
-                variant="h1"
-                color="white"
-                className="mb-2.5 md:mb-8 lg:mb-12 text-3xl md:text-5xl lg:text-8xl"
-              >
-                {s.title}
-              </Typography>
-              <Typography
-                variant="lead"
-                color="white"
-                className="mb-2.5 md:mb-8 lg:mb-12 text-sm md:text-xl lg:text-3xl"
-              >
-                {s.description}
-              </Typography>
-              <button className="px-3 md:px-6 py-1.5 md:py-3 text-xs md:text-base bg-secondary hover:bg-white text-primary font-semibold rounded-full shadow">
-                Explore Our Services
-              </button>
-            </div>
+        <div
+          key={i}
+          className={`py-12 md:py-24 flex justify-center items-center  bg-gradient-to-r ${
+            i % 2 !== 0
+              ? "from-primary via-[#34327D] to-primary"
+              : "from-blue-900 via-blue-400 to-blue-900"
+          }`}
+        >
+          <div className="text-center w-3/4 lg:w-3/5">
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-2.5 md:mb-8 lg:mb-12 text-3xl md:text-5xl lg:text-8xl"
+            >
+              {s.title}
+            </Typography>
+            <Typography
+              variant="lead"
+              color="white"
+              className="mb-2.5 md:mb-8 lg:mb-12 text-sm md:text-xl lg:text-3xl"
+            >
+              {s.description}
+            </Typography>
+            <button className="px-3 md:px-6 py-1.5 md:py-3 text-xs md:text-base bg-secondary hover:bg-white text-primary font-semibold rounded-full shadow">
+              Explore Our Services
+            </button>
           </div>
         </div>
       ))}
