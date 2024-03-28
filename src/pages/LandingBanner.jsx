222237c49eaeaba8e7d5bac4df7d49328617c5ea
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2024-03-29 03:26:33 +0600

    Landing banner edited and dummy image removed

--- src/pages/LandingBanner.jsx
+++ src/pages/LandingBanner.jsx
@@ -1,38 +1,32 @@
 import { Carousel, Typography } from "@material-tailwind/react";
-import sliderImage1 from "../assets/banner/loan_slider_image (1).jpg";
-import sliderImage2 from "../assets/banner/loan_slider_image (2).jpg";
-import sliderImage3 from "../assets/banner/loan_slider_image (3).jpg";
 
 export default function LandingBanner() {
   // sliders content
   const sliders = [
     {
-      banner: sliderImage1,
       title: "Unlock Your Dream Home",
       description:
         "Explore flexible loan options tailored to your needs, turning your dream of homeownership into reality.",
     },
 
     {
-      banner: sliderImage2,
       title: "Maximize Your Business",
       description:
         "Discover competitive rates and refinancing solutions to optimize your finances and save big on your mortgage.",
     },
 
     {
-      banner: sliderImage3,
       title: "Invest Wisely, Grow Securely",
       description:
         "Explore investment property loans designed to help you build wealth and secure your financial future with confidence.",
     },
   ];
   return (
-    <Carousel className="lg:h-[80vh]" navigation={false}>
+    <Carousel navigation={false}>
       {sliders.map((s, i) => (
         <div
           key={i}
-          className={`py-12 md:py-24 flex justify-center items-center  bg-gradient-to-r ${
+          className={`flex justify-center items-center  bg-gradient-to-r min-h-[60vh] lg:min-h-[80vh] ${
             i % 2 !== 0
               ? "from-primary via-[#34327D] to-primary"
               : "from-blue-900 via-blue-400 to-blue-900"
@@ -42,14 +36,14 @@ export default function LandingBanner() {
             <Typography
               variant="h1"
               color="white"
-              className="mb-2.5 md:mb-8 lg:mb-12 text-3xl md:text-5xl lg:text-8xl"
+              className="text-3xl md:text-5xl lg:text-8xl"
             >
               {s.title}
             </Typography>
             <Typography
               variant="lead"
               color="white"
-              className="mb-2.5 md:mb-8 lg:mb-12 text-sm md:text-xl lg:text-3xl"
+              className="my-5 md:my-10 text-sm md:text-xl lg:text-3xl"
             >
               {s.description}
             </Typography>
